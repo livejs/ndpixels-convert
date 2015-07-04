893c7954285dea2d3006ebb59936d3bb42a64506
Michael Williams <dinosaur@riseup.net>
2015-07-04 18:09:17 +1200

    do a more naive approach without cwise

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var converters = require('color-convert')
-var cwise = require('cwise')
+//var cwise = require('cwise')
 var zeros = require('zeros')
 var fs = require('fs')
 
-// TODO use cwise
-// but how, if we're using color-convert?
-//
 
 module.exports = function (from, to) {
+  /*
+
+  // TODO use cwise
+  // but how, if we're using color-convert?
+  //
   var pre = fs.readFileSync(
     require.resolve('color-convert/conversions')
   ).toString().replace('module.exports', 'this.convert')
@@ -21,6 +23,15 @@ module.exports = function (from, to) {
       b = this.convert(a)
     }
   })
+  */
+
+  var conversions = require('color-convert')
+  var fn = function convertData (input, output) {
+    Object.keys(input).forEach(function (i) {
+      output[i] = conversions[from][to](input[i])
+    })
+  }
+
   return function (input, output) {
     if (arguments.length < 2) {
       output = zeros(input.shape, input.dtype)
